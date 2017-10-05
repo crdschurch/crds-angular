06fd0115f9d4f8650850dd6282d3842d9e2e6493
Jim Kriz <jim.kriz@ingagepartners.com>
2017-10-05 09:52:20 -0400

    US9525: Remove erroneous /give

--- crossroads.net/core/embed/embed.controller.js
+++ crossroads.net/core/embed/embed.controller.js
@@ -46,7 +46,7 @@ export default class EmbedController {
         this.baseUrl = 'https://embeddemo.crossroads.net';
         break;
       case 'local':
-        this.baseUrl = 'http://local.crossroads.net:8080/give';
+        this.baseUrl = 'http://local.crossroads.net:8080';
         break;
       default:
         this.baseUrl = 'https://embed.crossroads.net';
