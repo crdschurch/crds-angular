5fc5048b729cfed9a7aae88bf12fde5366946cb0
Chris Branch <chris.branch@ingagepartners.com>
2017-10-17 11:11:41 -0400

    US10307 - Better index for dp_Selected_Records

---
+++ CI/SQL/20171017_104000_US10307-new-index-for-selected-records.sql
@@ -0,0 +1,10 @@
+﻿USE [MinistryPlatform]
+GO
+
+-- disable existing MP index
+ALTER INDEX IX_Selected_Records_Record_ID ON dp_Selected_Records DISABLE;
+GO
+
+-- add new index that covers more queries
+CREATE INDEX IX_dp_Selected_Records__SelectionID_RecordID ON dp_Selected_Records(Selection_ID, Record_ID);
+GO
