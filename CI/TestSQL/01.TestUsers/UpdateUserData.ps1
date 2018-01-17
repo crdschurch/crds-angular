30bdfe7fefc03b9af288c3e45f88e599d238bd4d
Sarah Henney <shenney@callibrity.com>
2018-01-17 13:13:28 -0500

    Renamed some files changed updateuserdata script

--- CI/TestSQL/01.TestUsers/UpdateUserData.ps1
+++ CI/TestSQL/01.TestUsers/UpdateUserData.ps1
@@ -10,25 +10,40 @@ param (
  $exitCode = 0
  $SQLCommonParams = @("-U", $DBUser, "-P", $DBPassword, "-S", $DBServer, "-b")
  
+ $connection = new-object System.Data.SqlClient.SqlConnection
+ $connection.ConnectionString = $SQLCommonParams #not sure if this is correct?
+ 
+ #this may need to be done in the loop
+ $command = $connection.CreateCommand()
+ $command.CommandText = "EXEC [MinistryPlatform].[dbo].[cr_QA_Update_Contact] @email, @middle_name, @dob, @gender, @marital_status, @household_position, @mobile_phone, @company_phone, @prefix"
+ 
  foreach($user in $contactDataList)
 {
 	if(![string]::IsNullOrEmpty($user.User_Email))
 	{
         $email = $user.User_Email
-		$middle = $user.Middle_Name
-		$dob = $user.Date_of_Birth
-		$gender = $user.Gender_ID
-		$marital_status = $user.Marital_Status_ID
-		$household_position = $user.Household_Position_ID
-		$mobile_phone = $user.Mobile_Phone
-		$company_phone = $user.Company_Phone
-		$prefix = $user.Prefix_ID
-		$output = & $SQLcmd @SQLCommonParams -Q """EXEC [MinistryPlatform].[dbo].[cr_QA_Update_Contact] '$email', '$middle', $dob, $gender, 
-			$marital_status, $household_position, '$mobile_phone', '$company_phone', $prefix"""
+		
+		$command.Parameters.AddWithValue("@email", $email) | Out-Null
+		$command.Parameters.AddWithValue("@middle_name", $user.Middle_Name) | Out-Null
+		$command.Parameters.AddWithValue("@dob", $user.Date_of_Birth) | Out-Null
+		$command.Parameters.AddWithValue("@gender", $user.Gender_ID) | Out-Null
+		$command.Parameters.AddWithValue("@marital_status", $user.Marital_Status_ID) | Out-Null
+		$command.Parameters.AddWithValue("@household_position", $user.Household_Position_ID) | Out-Null
+		$command.Parameters.AddWithValue("@mobile_phone", $user.Mobile_Phone) | Out-Null
+		$command.Parameters.AddWithValue("@company_phone", $user.Company_Phone) | Out-Null
+		$command.Parameters.AddWithValue("@prefix", $user.Prefix_ID) | Out-Null
+		
+		$adapter = new-object System.Data.SqlClient.SqlDataAdapter $command
+		$dataset = new-object System.Data.Dataset
+		
+		Write-Host $adapter.Fill($dataset) ' contact data has been added.'#debug
+		
+		#$output = & $SQLcmd @SQLCommonParams -Q """EXEC [MinistryPlatform].[dbo].[cr_QA_Update_Contact] '$email', '$middle', $dob, $gender, $marital_status, $household_position, '$mobile_phone', '$company_phone', $prefix"""
+		
 		
 		if($LASTEXITCODE -ne 0){
-				write-host "User: "$email
-				write-host "Error: "$output
+				write-host "There was an error updating user: "$email
+				#write-host "Error: "$output
 				$exitCode = $LASTEXITCODE
 			}
 	}
