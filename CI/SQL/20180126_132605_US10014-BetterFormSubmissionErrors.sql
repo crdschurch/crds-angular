acc2c946cf663c7581d2589cc1a871439da2480f
Sandi Ritter <sandi.ritter@ingagepartners.com>
2018-01-26 13:27:28 -0500

    new email template for form submission errors

---
+++ CI/SQL/20180126_132605_US10014-BetterFormSubmissionErrors.sql
@@ -0,0 +1,30 @@
+USE MinistryPlatform
+GO
+
+DECLARE @Template_ID int = 2035;
+DECLARE @From_Contact_ID int = 1;
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[dp_Communications] WHERE [Communication_ID] = @Template_ID)
+BEGIN
+	SET IDENTITY_INSERT [dbo].[dp_Communications] ON;
+	INSERT INTO [dbo].[dp_Communications] (
+		 [Communication_ID]
+		,[Author_User_ID]
+		,[Subject]
+		,[Body]
+		,[Domain_ID]
+		,[From_Contact]
+		,[Reply_to_Contact]
+		,[Template]
+	) VALUES (
+		 @Template_ID
+		,5
+		,N'[Product_Name] Form Submission Failed'
+		,N'A form submisison for [Product_Name] has failed. Details are below: <div><br /></div><div>User Name: [User_Name]</div><div><br /></div><div>Submission Date: [Submission_Date]</div><div><br /></div><div>Form Data:[Submission_Data]</div><div><br /></div>'
+		,1
+		,@From_Contact_ID
+		,@From_Contact_ID
+		,1
+	);
+	SET IDENTITY_INSERT [dbo].[dp_Communications] OFF;
+END
