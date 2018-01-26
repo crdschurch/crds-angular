85d1bbf76ec661122ceab575403d542f07b098e8
AJ Colubiale <aj.colub@thrivecincinnati.com>
2018-01-26 15:40:54 -0500

    pull in recipient first + last name

--- Gateway/MinistryPlatform.Translation/Repositories/InvoiceRepository.cs
+++ Gateway/MinistryPlatform.Translation/Repositories/InvoiceRepository.cs
@@ -62,7 +62,12 @@ namespace MinistryPlatform.Translation.Repositories
         public MpInvoiceDetail GetInvoiceDetailForInvoice(int invoiceId)
         {
             var apiToken = _apiUserRepository.GetToken();
-            return _ministryPlatformRest.UsingAuthenticationToken(apiToken).Get<MpInvoiceDetail>("Invoice_Detail", invoiceId, $"Recipient_Contact_ID_Table.[Contact_ID], Event_Participant_ID_Table_Event_ID_Table.[Event_ID], Invoice_Detail.[Item_Quantity], Invoice_Detail.[Line_Total], Product_ID_Table.[Product_ID], Product_Option_Price_ID_Table.[Product_Option_Price_ID], Invoice_Detail.[Item_Note], Recipient_Contact_ID_Table.[Display_Name] as [Recipient_Name]");
+            return _ministryPlatformRest.UsingAuthenticationToken(apiToken).Get<MpInvoiceDetail>("Invoice_Detail", invoiceId, $"Recipient_Contact_ID_Table.[Contact_ID], " +
+                                                                                                                              $"Event_Participant_ID_Table_Event_ID_Table.[Event_ID], " +
+                                                                                                                              $"Invoice_Detail.[Item_Quantity], Invoice_Detail.[Line_Total], " +
+                                                                                                                              $"Product_ID_Table.[Product_ID], Product_Option_Price_ID_Table.[Product_Option_Price_ID], " +
+                                                                                                                              $"Invoice_Detail.[Item_Note], " +
+                                                                                                                              $"Recipient_Contact_ID_Table.[First_Name] + ' ' + Recipient_Contact_ID_Table.[Last_Name] AS [Recipient_Name]");
         }
 
         public bool CreateInvoiceAndDetail(int productId, int? productOptionPriceId, int purchaserContactId, int recipientContactId, int eventParticipantId)
