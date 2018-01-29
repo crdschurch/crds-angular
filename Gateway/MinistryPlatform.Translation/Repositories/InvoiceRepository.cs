38e1b8df22096d950a7285d70299d2a6208cb33e
AJ Colubiale <aj.colub@thrivecincinnati.com>
2018-01-29 11:19:47 -0500

    use search instead of get

--- Gateway/MinistryPlatform.Translation/Repositories/InvoiceRepository.cs
+++ Gateway/MinistryPlatform.Translation/Repositories/InvoiceRepository.cs
@@ -62,12 +62,16 @@ namespace MinistryPlatform.Translation.Repositories
         public MpInvoiceDetail GetInvoiceDetailForInvoice(int invoiceId)
         {
             var apiToken = _apiUserRepository.GetToken();
-            return _ministryPlatformRest.UsingAuthenticationToken(apiToken).Get<MpInvoiceDetail>("Invoice_Detail", invoiceId, $"Recipient_Contact_ID_Table.[Contact_ID], " +
-                                                                                                                              $"Event_Participant_ID_Table_Event_ID_Table.[Event_ID], " +
-                                                                                                                              $"Invoice_Detail.[Item_Quantity], Invoice_Detail.[Line_Total], " +
-                                                                                                                              $"Product_ID_Table.[Product_ID], Product_Option_Price_ID_Table.[Product_Option_Price_ID], " +
-                                                                                                                              $"Invoice_Detail.[Item_Note], " +
-                                                                                                                              $"Recipient_Contact_ID_Table.[Nickname] + ' ' + Recipient_Contact_ID_Table.[Last_Name] AS [Recipient_Name]");
+            return _ministryPlatformRest.UsingAuthenticationToken(apiToken).Search<MpInvoiceDetail>("Invoice_Deatils", $"Invoice_ID_Table.[Invoice_ID]={invoiceId},", 
+                                                                                                    $"Invoice_ID_Table.[Invoice_ID], " +
+                                                                                                    $"Recipient_Contact_ID_Table.[Contact_ID], " +
+                                                                                                    $"Event_Participant_ID_Table_Event_ID_Table.[Event_ID], " +
+                                                                                                    $"Invoice_Detail.[Item_Quantity], " +
+                                                                                                    $"Invoice_Detail.[Line_Total], " +
+                                                                                                    $"Product_ID_Table.[Product_ID], " +
+                                                                                                    $"Product_Option_Price_ID_Table.[Product_Option_Price_ID], " +
+                                                                                                    $"Invoice_Detail.[Item_Note], " +
+                                                                                                    $"Recipient_Contact_ID_Table.[Nickname] + ' ' + Recipient_Contact_ID_Table.[Last_Name] AS [Recipient_Name]", null);
         }
 
         public bool CreateInvoiceAndDetail(int productId, int? productOptionPriceId, int purchaserContactId, int recipientContactId, int eventParticipantId)
