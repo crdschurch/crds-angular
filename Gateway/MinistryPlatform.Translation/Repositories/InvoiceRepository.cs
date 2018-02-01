f7e033e19c99015d4442271b176e3129d298e9ba
Sandi Ritter <sandi.ritter@ingagepartners.com>
2018-01-31 22:10:32 -0500

    make it prettier

--- Gateway/MinistryPlatform.Translation/Repositories/InvoiceRepository.cs
+++ Gateway/MinistryPlatform.Translation/Repositories/InvoiceRepository.cs
@@ -61,14 +61,16 @@ namespace MinistryPlatform.Translation.Repositories
 
     public MpInvoiceDetail GetInvoiceDetailForInvoice(int invoiceId)
     {
-      var filter = $"Invoice_ID_Table.[Invoice_ID] = {invoiceId}";
-
       var apiToken = _apiUserRepository.GetToken();
-      var cols =$"Invoice_Detail.[Invoice_Detail_ID], Invoice_ID_Table_Invoice_Status_ID_Table.[Invoice_Status_ID] , Invoice_ID_Table.[Invoice_ID] , Recipient_Contact_ID_Table.[Contact_ID] , Event_Participant_ID_Table.[Event_Participant_ID] , Invoice_Detail.[Item_Quantity] , Invoice_Detail.[Line_Total] , Product_ID_Table.[Product_ID] , Product_Option_Price_ID_Table.[Product_Option_Price_ID] , Invoice_Detail.[Item_Note] , Recipient_Contact_ID_Table.[Nickname] + ' ' + Recipient_Contact_ID_Table.[Last_Name] AS[Recipient_Name], Invoice_Detail.[Recipient_Email] , Invoice_Detail.[Recipient_Phone]";
+      var filter = $"Invoice_ID_Table.[Invoice_ID] = {invoiceId}";  
+      var cols =$"Invoice_Detail.[Invoice_Detail_ID], Invoice_ID_Table_Invoice_Status_ID_Table.[Invoice_Status_ID], Invoice_ID_Table.[Invoice_ID]," +
+                $"Recipient_Contact_ID_Table.[Contact_ID], Event_Participant_ID_Table.[Event_Participant_ID], Invoice_Detail.[Item_Quantity]," +
+                $"Invoice_Detail.[Line_Total], Product_ID_Table.[Product_ID], Product_Option_Price_ID_Table.[Product_Option_Price_ID]," +
+                $"Invoice_Detail.[Item_Note], Recipient_Contact_ID_Table.[Nickname] + ' ' + Recipient_Contact_ID_Table.[Last_Name] AS [Recipient_Name]," +
+                $"Invoice_Detail.[Recipient_Email] , Invoice_Detail.[Recipient_Phone]";
 
-      var asdf =  _ministryPlatformRest.UsingAuthenticationToken(apiToken).SearchTable<MpInvoiceDetail>($"Invoice_Detail",filter, cols).FirstOrDefault();
-           // var asdf = _ministryPlatformRest.UsingAuthenticationToken(apiToken).Search<MpInvoiceDetail>(filter).FirstOrDefault();
-            return asdf;
+      return _ministryPlatformRest.UsingAuthenticationToken(apiToken).SearchTable<MpInvoiceDetail>($"Invoice_Detail",filter, cols).FirstOrDefault();          
+     
     }
 
     public bool CreateInvoiceAndDetail(int productId, int? productOptionPriceId, int purchaserContactId, int recipientContactId, int eventParticipantId)
