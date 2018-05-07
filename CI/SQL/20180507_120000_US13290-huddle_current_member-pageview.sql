1c6d7463be5fcafbebbeae3a4fc80616ea3c23c5
Phil Lachmann <phil.lachmann@ingagepartners.com>
2018-05-07 13:34:25 -0400

    huddle for page views

---
+++ CI/SQL/20180507_120000_US13290-huddle_current_member-pageview.sql
@@ -0,0 +1,18 @@
+USE MinistryPlatform
+GO
+
+DECLARE @PAGEVIEWID INTEGER = 1132;
+
+IF NOT EXISTS(SELECT * FROM dp_Page_Views WHERE Page_View_ID = @PAGEVIEWID )
+BEGIN
+  SET IDENTITY_INSERT dp_Page_Views ON
+  INSERT INTO dp_Page_Views(Page_View_ID, View_Title, Page_ID, Field_List, View_Clause)
+	  VALUES(@PAGEVIEWID,
+	         'Huddle - Current Members', 
+			 316,
+			 'Group_ID_Table.[Group_Name] AS [Group Name], Group_Role_ID_Table.[Role_Title] AS [Role Title] ,  Group_ID_Table_Congregation_ID_Table.[Congregation_Name] AS [Congregation Name] , Participant_ID_Table_Contact_ID_Table.[Display_Name] AS [Display Name] , Participant_ID_Table_Contact_ID_Table.[Email_Address] AS [Email Address],(SELECT TOP 1 Attributes.Attribute_Name FROM Contact_Attributes, Attributes where Contact_Attributes.Attribute_ID=Attributes.Attribute_ID AND Contact_Attributes.Contact_ID=Participant_ID_Table_Contact_ID_Table.[Contact_ID] and Attributes.Attribute_Type_ID = 1005) as [Generation], Participant_ID_Table_Huddle_Status_ID_Table.[Huddle_Status] AS [Huddle Status] ',
+			 'Group_Participants.[End_Date] IS NULL  AND Group_ID_Table_Group_Type_ID_Table.[Group_Type_ID] = ''31'' '
+			);
+  SET IDENTITY_INSERT dp_Page_Views OFF
+END
+GO
