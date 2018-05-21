b75d65464df5705fa31bd4fec1e53dfc8205a87c
doug <dshannon@gmail.com>
2018-05-21 13:53:31 -0400

    looking at correct header noew

--- crossroads.net/core/components/interceptor.js
+++ crossroads.net/core/components/interceptor.js
@@ -1,36 +1,40 @@
-(function () {
-  'use strict';
+(function() {
+  "use strict";
 
-  const cookieNames = require('crds-constants').COOKIES;
-  angular.module('crossroads.core').factory('InterceptorFactory', InterceptorFactory);
+  const cookieNames = require("crds-constants").COOKIES;
+  angular
+    .module("crossroads.core")
+    .factory("InterceptorFactory", InterceptorFactory);
 
-  InterceptorFactory.$inject = ['$injector'];
+  InterceptorFactory.$inject = ["$injector"];
 
   function InterceptorFactory($injector) {
     return {
-      request: function (config) {
+      request: function(config) {
         // Make sure Crds-Api-Key is set on all requests using $http,
         // even those that explicitly specify other headers
-        if (config.headers && (config.headers['Crds-Api-Key'] === undefined ||
-          config.headers['Crds-Api-Key'].length === 0) &&
-          __CROSSROADS_API_TOKEN__.length > 0) {
-          config.headers['Crds-Api-Key'] = __CROSSROADS_API_TOKEN__;
+        if (
+          config.headers &&
+          (config.headers["Crds-Api-Key"] === undefined ||
+            config.headers["Crds-Api-Key"].length === 0) &&
+          __CROSSROADS_API_TOKEN__.length > 0
+        ) {
+          config.headers["Crds-Api-Key"] = __CROSSROADS_API_TOKEN__;
         }
         return config;
       },
 
-      response: function (response) {
+      response: function(response) {
         const url = response.config.url;
-        const isHttp = url.startsWith('http://') || url.startsWith('https://');
+        const isHttp = url.startsWith("http://") || url.startsWith("https://");
         const expDate = new Date();
         const sessionLength = 1800000;
         // debugger;
         expDate.setTime(expDate.getTime() + sessionLength);
-        var Session = $injector.get('Session');
-        if (response.headers(cookieNames.SESSION_ID) && isHttp) {
+        var Session = $injector.get("Session");
+        if (response.headers("sessionId") && isHttp) {
           Session.getNewSessionFromHeaders(response, sessionLength, expDate);
-        }
-        else if (isHttp) {
+        } else if (isHttp) {
           Session.updateSessionExpiration(sessionLength);
         }
 
@@ -39,11 +43,10 @@
     };
   }
 
-  var app = angular.module('crossroads.core');
+  var app = angular.module("crossroads.core");
   app.config(AppConfig);
-  AppConfig.$inject = ['$httpProvider'];
+  AppConfig.$inject = ["$httpProvider"];
   function AppConfig($httpProvider) {
-    $httpProvider.interceptors.push('InterceptorFactory');
+    $httpProvider.interceptors.push("InterceptorFactory");
   }
-
 })();
