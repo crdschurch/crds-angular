43b66a177654afc8e54395aa8fbbdf3433c5dc07
candrews-thrive <chris.andrews@thrivecincinnati.com>
2018-06-20 16:28:15 -0400

    Address pr comments

--- CI/TestSQL/StandardScripts/02.TestData/05.Opportunity+Response/Stored Procs/01.CreateData_CreateOpportunity_StoredProc.sql
+++ CI/TestSQL/StandardScripts/02.TestData/05.Opportunity+Response/Stored Procs/01.CreateData_CreateOpportunity_StoredProc.sql
@@ -140,8 +140,8 @@ BEGIN
 	IF @opportunity_id is null
 	BEGIN
 		INSERT INTO [dbo].Opportunities
-		(Opportunity_Title,Group_Role_ID,Program_ID ,Visibility_Level_ID,Contact_Person,Publish_Date ,Send_Reminder ,Domain_ID ,Opportunity_Subtitle) VALUES
-		(@opportunity_name,@role_id     ,@program_id,@visibility_level  ,@contact_id   ,@publish_date,@send_reminder,1         ,@opportunity_Subtitle);
+		(Opportunity_Title,Group_Role_ID,Program_ID ,Visibility_Level_ID,Contact_Person,Publish_Date ,Send_Reminder ,Domain_ID) VALUES
+		(@opportunity_name,@role_id     ,@program_id,@visibility_level  ,@contact_id   ,@publish_date,@send_reminder,1        );
 
 		SET @opportunity_id = SCOPE_IDENTITY();
 	END;
@@ -168,7 +168,7 @@ BEGIN
 		Reminder_Days_Prior = @reminder_days_prior,
 		Description = @description,  
 		Reminder_Template = @reminder_template_id,
-		Opportunity_Subtitle = @opportunity_Subtitle,
+		Opportunity_Subtitle = @opportunity_subtitle,
 		WHERE Opportunity_ID = @opportunity_id;
 	END;
 END
