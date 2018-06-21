5652c9c9b1445802d341def00505cc88c326a32b
ilpadre <ilpadre1953@gmail.com>
2018-06-21 13:08:49 -0400

    US12916 Code review changes

--- CI/SQL/2018_06_20_130000_US12916-create-quarterly-giving-statement-function.sql
+++ CI/SQL/2018_06_20_130000_US12916-create-quarterly-giving-statement-function.sql
@@ -1,10 +1,8 @@
 USE [MinistryPlatform]
 GO
-
-/****** Object:  UserDefinedFunction [dbo].[crds_QuarterlyGivingStatementDonors]    Script Date: 6/19/2018 1:07:02 PM ******/
+/****** Object:  UserDefinedFunction [dbo].[crds_QuarterlyGivingStatementDonors]    Script Date: 6/21/2018 10:26:41 AM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
 
@@ -20,73 +18,82 @@ RETURNS @DonorsTable TABLE(		ContactId	int,
 						 )
 AS
      BEGIN
-	 	DECLARE @BeginDate DATETIME = 
-			CASE WHEN @Quarter < 4 THEN DATEFROMPARTS (YEAR(getdate()), 1, 1 )--Q1-Q3 always show current year
-				 WHEN @Quarter = 4 AND MONTH(getdate()) >= 7 THEN DATEFROMPARTS (YEAR(getdate()), 1, 1 )--show current year
-				 WHEN @Quarter = 4 AND MONTH(getdate()) < 7 THEN DATEFROMPARTS (YEAR(getdate())-1, 1, 1 )--show previous year
-			END
-		DECLARE @EndDate DATETIME =
-			CASE WHEN @Quarter = 1 THEN DATEFROMPARTS (YEAR(getdate()), 3, 31 )
-				 WHEN @Quarter = 2 THEN DATEFROMPARTS (YEAR(getdate()), 6, 30 )
-			     WHEN @Quarter = 3 THEN DATEFROMPARTS (YEAR(getdate()), 9, 30 )
-				 WHEN @Quarter = 4 AND MONTH(getdate()) >=7 THEN DATEFROMPARTS (YEAR(getdate()), 12, 31 )--show current year
-				 WHEN @Quarter = 4 AND MONTH(getdate()) < 7 THEN DATEFROMPARTS (YEAR(getdate())-1, 12, 31 )--show previous year
-			END;
+        DECLARE @CurrentDate DATETIME = GETDATE();
+        DECLARE @Year INT = YEAR(@CurrentDate);
 
-		INSERT INTO @DonorsTable
-		SELECT c.Contact_ID as ContactID, 
-				d.Donor_ID as DonorId,
-				c.Display_Name as DisplayName,
-				sm.Statement_Method as StatementMethod,
-				c.Email_Address as EmailAddress,
-				co.Congregation_Name as Congregation
-		FROM Contacts c
-			inner join Donors d on d.Donor_ID = c.Donor_Record
-			inner join Statement_Methods sm on sm.Statement_Method_ID = d.Statement_Method_ID
-			left outer join Households h on c.Household_ID = h.Household_ID
-			left outer join Congregations co on co.Congregation_ID = h.Congregation_ID
-		WHERE c.Contact_ID in
-		(
-			SELECT c.Contact_ID
-			FROM dbo.Contacts c
-				inner join dbo.Donors d on d.Donor_ID = c.Donor_Record
-			WHERE Exists (SELECT 1 FROM dbo.Donations ds						
-							WHERE ds.Donor_ID = d.Donor_ID
-								and ds.Donation_Status_ID in (2)
-								and ds.Donation_Date >= @BeginDate 
-								and ds.Donation_Date <= @EndDate
-							)
-			)
+        -- for Q4, show last year's data during the 1st half of the year
+        IF @Quarter = 4 AND MONTH(@CurrentDate) < 7
+        BEGIN
+            SET @Year = @Year - 1;
+        END
 
-		INSERT INTO @DonorsTable
-		SELECT c.Contact_ID as ContactID, 
-				d.Donor_ID as DonorId,
-				c.Display_Name as DisplayName,
-				sm.Statement_Method as StatementMethod,
-				c.Email_Address as EmailAddress,
-				co.Congregation_Name as Congregation
-		FROM Contacts c
-			inner join Donors d on d.Donor_ID = c.Donor_Record
-			inner join Statement_Methods sm on sm.Statement_Method_ID = d.Statement_Method_ID
-			inner join dbo.Contact_Relationships cr on cr.Contact_ID = c.Contact_ID and cr.Relationship_ID = 42
-			left outer join Households h on c.Household_ID = h.Household_ID
-			left outer join Congregations co on co.Congregation_ID = h.Congregation_ID
-		where c.Contact_ID not in
-			(
-				select c.Contact_ID
-				from dbo.Contacts c
-					inner join dbo.Donors d on d.Donor_ID = c.Donor_Record
-				where Exists (select 1 from dbo.Donations ds						
-							where ds.Donor_ID = d.Donor_ID
-								and ds.Donation_Status_ID in (2)
-								and ds.Donation_Date >= @BeginDate 
-								and ds.Donation_Date <= @EndDate
-					)
-		)		
+        DECLARE @BeginDate DATETIME = DATEFROMPARTS(@Year, 1, 1);
+        DECLARE @EndDate DATETIME =
+            CASE WHEN @Quarter = 1 THEN DATEADD(DAY, 1, DATEFROMPARTS(@Year, 3, 31))
+                 WHEN @Quarter = 2 THEN DATEADD(DAY, 1, DATEFROMPARTS(@Year, 6, 30))
+                 WHEN @Quarter = 3 THEN DATEADD(DAY, 1, DATEFROMPARTS(@Year, 9, 30))
+                 WHEN @Quarter = 4 THEN DATEADD(DAY, 1, DATEFROMPARTS(@Year, 12, 31))
+            END;
+
+        DECLARE @DonorIDs TABLE (
+            Contact_ID INT NOT NULL,
+            Donor_ID INT NOT NULL,
+            PRIMARY KEY(Contact_ID)
+        );
+
+        -- list of unique donor IDs (with contact ID) that have given in the desired timeframe
+        INSERT INTO @DonorIDs
+            (Contact_ID, Donor_ID)
+        SELECT DISTINCT
+            do.Contact_ID, do.Donor_ID
+        FROM
+            dbo.Donors do
+            INNER JOIN dbo.Donations d on d.Donor_ID = do.Donor_ID
+        WHERE
+            d.Donation_Status_ID IN (2)
+            AND d.Donation_Date >= @BeginDate
+            AND d.Donation_Date < @EndDate
+        ;
+
+        -- givers
+        INSERT INTO @DonorsTable
+        SELECT
+            c.Contact_ID as ContactID,
+            do.Donor_ID as DonorId,
+            c.Display_Name as DisplayName,
+            sm.Statement_Method as StatementMethod,
+            c.Email_Address as EmailAddress,
+            co.Congregation_Name as Congregation
+        FROM
+            Contacts c
+            INNER JOIN @DonorIDs di on di.Contact_ID = c.Contact_ID
+            INNER JOIN Donors do on do.Donor_ID = di.Donor_ID
+            INNER JOIN Statement_Methods sm on sm.Statement_Method_ID = do.Statement_Method_ID
+            LEFT OUTER JOIN Households h on c.Household_ID = h.Household_ID
+            LEFT OUTER JOIN Congregations co on co.Congregation_ID = h.Congregation_ID
+        ;
+
+        -- co-givers who are not already in the givers list (i.e., did not donate)
+        INSERT INTO @DonorsTable
+        SELECT
+            c.Contact_ID as ContactID, 
+            do.Donor_ID as DonorId,
+            c.Display_Name as DisplayName,
+            sm.Statement_Method as StatementMethod,
+            c.Email_Address as EmailAddress,
+            co.Congregation_Name as Congregation
+        FROM
+            @DonorIDs di
+            INNER JOIN Contact_Relationships cr on cr.Contact_ID = di.Contact_ID and cr.Relationship_ID = 42
+            INNER JOIN Contacts c on c.Contact_ID = cr.Related_Contact_ID
+            INNER JOIN Donors do on do.Donor_ID = di.Donor_ID
+            INNER JOIN Statement_Methods sm on sm.Statement_Method_ID = do.Statement_Method_ID
+            LEFT OUTER JOIN Households h on c.Household_ID = h.Household_ID
+            LEFT OUTER JOIN Congregations co on co.Congregation_ID = h.Congregation_ID
+        WHERE
+            NOT EXISTS (SELECT 1 FROM @DonorIDs di WHERE c.Contact_ID = di.Contact_ID)
+        ;	
 
     RETURN;
     END;
-
-GO
-
 
