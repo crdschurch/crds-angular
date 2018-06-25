04eacbd08c3f4b55abd5572be7c0dce1d0181ef8
ilpadre <ilpadre1953@gmail.com>
2018-06-25 10:59:02 -0400

    US13565 tweaks to the query

--- CI/SQL/20180622_130100_US13565-create-co-giver-views.sql
+++ CI/SQL/20180622_130100_US13565-create-co-giver-views.sql
@@ -9,33 +9,21 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
-ALTER VIEW [dbo].[vw_crds_Cogiver_Exceptions]
+CREATE OR ALTER VIEW [dbo].[vw_crds_Cogiver_Exceptions]
 AS
--- Overlapping co-giver relationships
-	select cr1.Contact_ID as ContactId, c.Display_Name as DisplayName, 'Start and end dates overlap' as Exception
-	from dbo.contact_relationships cr1
-		inner join dbo.contact_relationships cr2 on cr1.contact_id = cr2.contact_id 
-		inner join dbo.Contacts c on c.Contact_ID = cr1.Contact_ID
-	where 
-		cr1.relationship_id = 42
-		and 
-		cr2.relationship_id = 42
-		and
-		cr1.Contact_Relationship_ID <> cr2.contact_relationship_id
-		and
-		(
-			(convert(date,cr2.start_date,101) >= convert(date,cr1.start_date,101) and 
-			convert(date,cr2.start_date,101) <= convert(date,cr1.end_date,101))
-			or
-			(convert(date,cr2.end_date,101) <= convert(date,cr1.end_date,101) and 
-			convert(date,cr2.end_date,101) >= convert(date,cr1.start_date,101))
-			or
-			(cr2.end_date is null and convert(date,cr1.end_date,101) > convert(date,cr2.start_date,101))
-			or
-			(cr1.end_date is null and cr2.end_date is null)
-			or
-			(cr1.end_date is null and cr2.start_date > cr1.start_date)
-		)
+	-- Overlapping co-giver relationships
+    select distinct
+        cr1.Contact_ID as ContactId,
+        c.Display_Name as DisplayName,
+        'Start and end dates overlap' as Exception
+    from
+        dbo.Contacts c
+        inner join dbo.Contact_Relationships cr1 on cr1.Contact_ID = c.Contact_ID and cr1.Relationship_ID = 42
+        inner join dbo.Contact_Relationships cr2 on cr2.Contact_ID = c.Contact_ID and cr2.Relationship_ID = 42
+    where 
+        cr1.Contact_Relationship_ID <> cr2.contact_relationship_id
+        and CONVERT(DATE,COALESCE(cr1.Start_Date, '9999-12-31')) < CONVERT(DATE,COALESCE(cr2.End_Date, '9999-12-31'))
+        and CONVERT(DATE, COALESCE(cr1.End_Date, '9999-12-31')) > CONVERT(DATE, COALESCE(cr2.Start_Date, '9999-12-31'))
 	UNION
 	--script to identify co-giver relationships where start and end dates are not matched in two related records
 	select cr1.contact_id as ContactId, c.Display_Name as DisplayName, 'Start and end dates do not match' as Exception
@@ -45,35 +33,32 @@ AS
 	where cr1.relationship_id = 42
 		and cr2.relationship_id = 42
 		and cr2.contact_id = cr1.related_contact_id
-		and (CONVERT(DATE,cr1.start_date) <> CONVERT(DATE,cr2.start_date) or CONVERT(DATE,cr1.end_date) <> CONVERT(DATE, cr2.end_date))
+		and (CONVERT(DATE, coalesce(cr1.Start_Date, '9999-12-31')) <> CONVERT(DATE, coalesce(cr2.Start_Date, '9999-12-31')) or CONVERT(DATE, coalesce(cr1.End_Date, '9999-12-31')) <> CONVERT(DATE, coalesce(cr2.End_Date, '9999-12-31')))
 	UNION
 	--Script to identify co-giver contact relationships that have null start dates
 	select c1.Contact_ID as ContactId, c1.display_name as DisplayName, 'Cogiver has no start date' as Exception
 	from dbo.contact_relationships cr
 		join dbo.contacts c1 on cr.contact_id = c1.contact_id
-		join dbo.contacts c2 on cr.related_contact_id = c2.contact_id
 	where cr.relationship_id = 42 and cr.start_date is null 
 	UNION
 	--Script to identify co-giver contact relationships that have end dates before start dates
 	select c1.Contact_ID as ContactId, c1.display_name as DisplayName, 'End date prior to start date' as Exception
 	from dbo.contact_relationships cr
 		join dbo.contacts c1 on cr.contact_id = c1.contact_id
-		join dbo.contacts c2 on cr.related_contact_id = c2.contact_id
-	where cr.relationship_id = 42 and cr.start_date > cr.end_date 
+	where cr.relationship_id = 42 and CONVERT(DATE, coalesce(cr.Start_Date, '1990-12-31')) > CONVERT(DATE, coalesce(cr.End_Date, '9999-12-31')) 
 	UNION
 	--script to identify unmatched co-giver records
-	select cr.Contact_ID as ContactId, c.Display_Name as DisplayName, 'No reciprocal cogiver relationship' as Exception
-	from dbo.contact_relationships cr
-		inner join dbo.Contacts c on c.Contact_ID = cr.Contact_ID
-	where cr.relationship_id = 42
-		and cr.End_Date is null
-		and cr.Contact_ID not in 
-			(	select Related_Contact_ID 
-				from dbo.Contact_Relationships 
-				where Relationship_ID = 42 
-					AND End_Date is NULL
-					--AND CONVERT(DATE, cr.Start_Date) = CONVERT(DATE, Start_Date) Don't think we care about Start_Date
-			)
+	select
+		c2.Contact_ID as ContactId,
+		c2.Display_Name as DisplayName,
+		'No reciprocal cogiver relationship' as Exception
+	from
+		dbo.Contacts c1
+			inner join dbo.Contact_Relationships cr1 on cr1.Contact_ID = c1.Contact_ID and cr1.Relationship_ID = 42
+			inner join dbo.Contacts c2 on c2.Contact_ID = cr1.Related_Contact_ID
+			left join dbo.Contact_Relationships cr2 on cr2.Contact_ID = c2.Contact_ID and cr2.Relationship_ID = 42 and cr2.Related_Contact_ID = c1.Contact_ID and CONVERT(DATE, coalesce(cr1.Start_Date, '9999-12-31')) = CONVERT(DATE, coalesce(cr2.Start_Date, '9999-12-31'))
+	where
+		cr2.Contact_ID IS NULL
 GO
 
 
