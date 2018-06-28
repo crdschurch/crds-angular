f748ba2ec3381d693c42c2be30cc2a28681243ba
ilpadre <ilpadre1953@gmail.com>
2018-06-28 16:03:10 -0400

    US14055 fix view clauses

--- CI/SQL/20180628_110000_US14055-add-views-to-giving-statement-pages.sql
+++ CI/SQL/20180628_110000_US14055-add-views-to-giving-statement-pages.sql
@@ -19,22 +19,22 @@ INSERT INTO [dbo].[dp_Page_Views]
            ,1018
            ,'List of contacts that gave in the first quarter of this year. Filter out Guest Giver, Cash Offerings, Stripe Finance, Anonymous, and Non Cash.'
            ,'ContactID, Relationship, DonorId, DisplayName, StatementMethod, EmailAddress, Congregation, Notes'
-           ,'DisplayName not like ''Cash Offering%'' AND DisplayName not like ''Guest Giver%'' AND DisplayName not like ''Anyonymous%'' AND DisplayName not like ''Non Cash%'' AND DisplayName not like ''Stripe%'''),
+           ,'StatementMethod not like ''No Statement%'' AND EmailAddress is not null'),
            ('Email Statements'
            ,1020
            ,'List of contacts that gave in thesSecond quarter of this year. Filter out Guest Giver, Cash Offerings, Stripe Finance, Anonymous, and Non Cash.'
            ,'ContactID, Relationship, DonorId, DisplayName, StatementMethod, EmailAddress, Congregation, Notes'
-           ,'DisplayName not like ''Cash Offering%'' AND DisplayName not like ''Guest Giver%'' AND DisplayName not like ''Anyonymous%'' AND DisplayName not like ''Non Cash%'' AND DisplayName not like ''Stripe%'''),
+           ,'StatementMethod not like ''No Statement%'' AND EmailAddress is not null'),
            ('Email Statements'
            ,1021
            ,'List of contacts that gave in the third quarter of this year. Filter out Guest Giver, Cash Offerings, Stripe Finance, Anonymous, and Non Cash.'
            ,'ContactID, Relationship, DonorId, DisplayName, StatementMethod, EmailAddress, Congregation, Notes'
-           ,'DisplayName not like ''Cash Offering%'' AND DisplayName not like ''Guest Giver%'' AND DisplayName not like ''Anyonymous%'' AND DisplayName not like ''Non Cash%'' AND DisplayName not like ''Stripe%'''),
+           ,'StatementMethod not like ''No Statement%'' AND EmailAddress is not null'),
            ('Email Statements'
            ,1022
            ,'List of contacts that gave in the fourth quarter of this year. Filter out Guest Giver, Cash Offerings, Stripe Finance, Anonymous, and Non Cash.'
            ,'ContactID, Relationship, DonorId, DisplayName, StatementMethod, EmailAddress, Congregation, Notes'
-           ,'DisplayName not like ''Cash Offering%'' AND DisplayName not like ''Guest Giver%'' AND DisplayName not like ''Anyonymous%'' AND DisplayName not like ''Non Cash%'' AND DisplayName not like ''Stripe%''')
+           ,'StatementMethod not like ''No Statement%'' AND EmailAddress is not null'),
 
 GO
 
@@ -49,21 +49,21 @@ INSERT INTO [dbo].[dp_Page_Views]
            ,1018
            ,'List of contacts that gave in the first quarter of this year and have a statement method of No Statement Required or that do not have an email address'
            ,'ContactID, Relationship, DonorId, DisplayName, StatementMethod, EmailAddress, Congregation, Notes'
-           ,'StatementMethod like ''No Statement%''  OR EmailAddress is null'),
+           ,'(StatementMethod like ''No Statement%'' OR (EmailAddress is null AND StatementMethod = ''Email/Online'')) AND NOT (DisplayName like ''Cash Offer%''  OR DisplayName like ''Guest Giver%'')'),
            ('No Statement/Exceptions'
            ,1020
            ,'List of contacts that gave in the second quarter of this year and have a statement method of No Statement Required or that do not have an email address'
            ,'ContactID, Relationship, DonorId, DisplayName, StatementMethod, EmailAddress, Congregation, Notes'
-           ,'StatementMethod like ''No Statement%''  OR EmailAddress is null'),
+           ,'(StatementMethod like ''No Statement%'' OR (EmailAddress is null AND StatementMethod = ''Email/Online'')) AND NOT (DisplayName like ''Cash Offer%''  OR DisplayName like ''Guest Giver%'')'),
            ('No Statement/Exceptions'
            ,1021
            ,'List of contacts that gave in the second quarter of this year and have a statement method of No Statement Required or that do not have an email address'
            ,'ContactID, Relationship, DonorId, DisplayName, StatementMethod, EmailAddress, Congregation, Notes'
-           ,'StatementMethod like ''No Statement%''  OR EmailAddress is null'),
+           ,'(StatementMethod like ''No Statement%'' OR (EmailAddress is null AND StatementMethod = ''Email/Online'')) AND NOT (DisplayName like ''Cash Offer%''  OR DisplayName like ''Guest Giver%'')'),
            ('No Statement/Exceptions'
            ,1022
            ,'List of contacts that gave in the second quarter of this year and have a statement method of No Statement Required or that do not have an email address'
-           ,'ContactID, Relationship, DonorId, DisplayName, StatementMethod, EmailAddress, Congregation, Notes'
+           ,'(StatementMethod like ''No Statement%'' OR (EmailAddress is null AND StatementMethod = ''Email/Online'')) AND NOT (DisplayName like ''Cash Offer%''  OR DisplayName like ''Guest Giver%'')'),
            ,'StatementMethod like ''No Statement%''  OR EmailAddress is null')
 
 GO
