044040ae518aa3f982bb1832631f509a4f3e75cf
candrews-thrive <chris.andrews@thrivecincinnati.com>
2018-07-17 17:44:30 -0400

    Added two scripts to create and delete testing printer map

---
+++ CI/TestSQL/CustomScripts/Reload/CreatePrinterMap.sql
@@ -0,0 +1,5 @@
+USE [MinistryPlatform]
+GO
+
+INSERT INTO cr_Printer_Maps (Printer_ID,Printer_Name       ,Computer_ID,Computer_Name      ,Domain_ID)
+VALUES                      (0         ,'(t+auto) Printer' ,0          ,'(t+auto) Computer',1)
