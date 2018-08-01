3696c03859cbffbe4ad1935696b016dc36a66422
Chris Branch <chris.branch@ingagepartners.com>
2018-08-01 13:23:06 -0400

    Update report_CRDS_Large_Donations with new co-giver rules

--- CI/SQL/20180801_130100_US14442-update-large-donations-sp.sql
+++ CI/SQL/20180801_130100_US14442-update-large-donations-sp.sql
@@ -1,7 +1,7 @@
 USE [MinistryPlatform]
 GO
 
-CREATE PROCEDURE [dbo].[report_CRDS_Large_Donations] 
+CREATE OR ALTER PROCEDURE [dbo].[report_CRDS_Large_Donations] 
      @startdate DATETIME,
      @enddate DATETIME,
      @programid as varchar(MAX),
@@ -16,39 +16,8 @@ BEGIN
 SET nocount ON;
 
 
-IF OBJECT_ID('tempdb..#ldcogivers') IS NOT NULL   
-    DROP TABLE #ldcogivers
 IF OBJECT_ID('tempdb..#largedonation') IS NOT NULL   
     DROP TABLE #largedonation
-IF OBJECT_ID('tempdb..#nonstandardhh') IS NOT NULL
-    DROP TABLE #nonstandardhh
-
-
---build cogiver/spouse model
-CREATE TABLE #ldcogivers
-(
-    cid1	int,
-    cbday   date,
-    cstmttype int,
-    cid2	int,
-    cgbday  date,
-    cgstmttype int,
-    hhid	int
-)
-
-
-insert into #ldcogivers
-    select distinct c1.contact_id,c1.Date_of_Birth,d1.statement_type_id, c2.contact_id, c2.Date_of_Birth,d2.statement_type_id,c1.household_id
-    FROM contacts c1 
-    JOIN contacts c2 on c1.household_id = c2.household_id
-    join donors d1 on d1.contact_id = c1.contact_id
-    join donors d2 on d2.contact_id = c2.contact_id
-    where c1.household_position_id = 1
-    and c2.household_position_id =1 --head of household
-    and (d1.statement_type_id = 2 -- family
-    or d2.Statement_Type_ID = 2)
-    and c1.contact_id <> c2.contact_id
-    and c1.Display_Name <> c2.Display_Name
 
 
 CREATE TABLE #largedonation
@@ -99,7 +68,7 @@ h.household_id
 , c.display_name
 , c.last_name
 , c.first_name
-, cg.cid2 as contact2
+, c2.contact_id as contact2
 , c2.display_name as cogiver
 , c2.last_name
 , c2.First_Name
@@ -117,8 +86,10 @@ join donation_distributions dd on dd.donation_id = don.donation_id
 left join programs p on p.program_id = dd.program_id
 left join donors d on d.donor_id = don.donor_id
 left join contacts c on c.contact_id=d.contact_id
-left join #ldcogivers cg on cg.cid1 = c.Contact_ID
-left join contacts c2 on c2.contact_id = cg.cid2
+left join contact_relationships cr on cr.Contact_ID = c.Contact_ID
+    and relationship_id = 42
+    and don.donation_date between cr.start_date and coalesce(cr.end_date, '9999-12-31')
+left join contacts c2 on c2.contact_id = cr.related_contact_id
 left join households h on h.household_id = c.household_id
 left join congregations AS con ON con.congregation_id = dd.Congregation_ID
 left join addresses a on a.address_id = h.address_id
@@ -168,28 +139,12 @@ update #largedonation set sdcity='' where sdcity is null
 update #largedonation set sdst='' where sdst is null
 update #largedonation set sdzip='' where sdzip is null
 
-CREATE TABLE #nonstandardhh
-(
-    hhid int
-)
-
---take out households with greater than 2 Heads of Households that are active
-insert into #nonstandardhh(hhid)
-    select c.household_id from contacts c 
-        where c.household_position_id = 1
-        and c.Contact_Status_ID = 1
-        group by c.household_id
-        having count(c.contact_id) > 2
-
-
 select distinct ld.amount as 'Distribution Amount', ld.donationid as 'Donation ID',ld.dondate as 'Donation Date',ld.donamount as 'Donation Amount', ld.donationstatus as 'Donation Status',ld.progname as 'Fund',ld.cdisplayname as 'Giver Name',
 ld.clname as 'Giver Last Name',ld.cfname as 'Giver First Name',ld.cgdisplayname as 'Spouse Name',ld.cglname as 'Spouse Last Name',ld.cgfname as 'Spouse First Name', ld.addr1 as 'Address1', ld.addr2 as 'Address2',ld.city as 'City',ld.st as 'State',ld.zip as 'Zip',ld.conname as 'Site Name',
 ld.sddisplayname as 'Soft Credit Donor Name',ld.sdlname as 'Soft Credit Donor Last Name',ld.sdfname as 'Soft Credit Donor First Name', ld.sdaddr1 as 'Soft Credit Address1',ld.sdaddr2 as 'Soft Credit Address2', ld.sdcity as 'Soft Credit City', ld.sdst as 'Soft Credit State',
 ld.sdzip as 'Soft Credit Zip'
 from #largedonation ld
-left join #nonstandardhh nshh on nshh.hhid = ld.hhid
-where nshh.hhid is null
-and ld.cdisplayname not like 'Offering%'
+where ld.cdisplayname not like 'Offering%'
 order by ld.cdisplayname,ld.progname,ld.dondate
 
 END
