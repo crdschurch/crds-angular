1e1ddeba3d4a156657dcb39d244cb57b8e970f6a
vchtel <VictorChtelmakh@gmail.com>
2018-08-31 14:43:31 -0400

    Write single attribute to Contact_Attributes, overwrite it if it changes. Some UI changes

---
+++ crossroads.net/app/trips/preferred-airport.service.js
@@ -0,0 +1,68 @@
+(function() {
+  'use strict';
+  module.exports = PreferredAirportService;
+
+  function PreferredAirportService() {
+    const consts = require('crds-constants');
+    const preferredAirportAttributeTypeId = consts.ATTRIBUTE_TYPE_IDS.PREFERRED_AIRPORT;
+
+    const COMMON_AIRPORT_NAMES = {
+      CVG: 'Cincinnati(CVG)',
+      LEX: 'Lexington(LEX)',
+      OTHER: 'Other'
+    };
+
+    return {
+      GetCommonAirportNames: function() {
+        return COMMON_AIRPORT_NAMES;
+      },
+
+      GetCommonAirportNameOrOther(airportName, commonAirportNames) {
+        var commonAirportName = undefined;
+
+        if (airportName === commonAirportNames.CVG || airportName === commonAirportNames.LEX){
+          commonAirportName = airportName;
+        } else if (airportName != null && airportName != ''){
+          commonAirportName = 'Other';
+        }
+        return commonAirportName;
+      },
+
+      GetPreferredAirportName(person) {
+        var preferredAirportName = undefined;
+
+        const hasPreferredAirportAttribute = person.singleAttributes[preferredAirportAttributeTypeId] != null;
+
+        if(hasPreferredAirportAttribute) {
+          preferredAirportName = person.singleAttributes[preferredAirportAttributeTypeId].notes;
+        }
+
+        return preferredAirportName;
+      },
+
+      SetAttributeNotesAndStartDate(preferredAirportAttribute, preferredAirportName) {
+        preferredAirportAttribute.notes = preferredAirportName;
+        preferredAirportAttribute.selected = true;
+        preferredAirportAttribute.startDate = new Date();
+
+        return preferredAirportAttribute;
+      },
+
+      DoesPersonHaveValidPreferredAirportNameAttributeSet(person) {
+        var idxOfPreferredAirportAttribute = person.attributeTypes[preferredAirportAttributeTypeId]
+          .attributes.findIndex(attr => attr.attributeId === consts.ATTRIBUTE_IDS.PREFERRED_AIRPORT_NAME);
+
+        var hasValidPreferredAirportAttribute = idxOfPreferredAirportAttribute !== -1;
+
+        var preferredAirportNameNotes = person.attributeTypes[preferredAirportAttributeTypeId].attributes[idxOfPreferredAirportAttribute].notes;
+
+        if(preferredAirportNameNotes == null) {
+          hasValidPreferredAirportAttribute = false;
+        }
+
+        return hasValidPreferredAirportAttribute;
+      }
+
+    };
+  }
+})();
--- crossroads.net/app/trips/travelInformation/travelInformation.controller.js
+++ crossroads.net/app/trips/travelInformation/travelInformation.controller.js
@@ -1,13 +1,17 @@
 // eslint-disable-next-line import/no-extraneous-dependencies,import/no-unresolved
-const attributeType = require('crds-constants').ATTRIBUTE_TYPE_IDS.FREQUENT_FLYERS;
+
+const consts = require('crds-constants');
+const frequentFlyerAttributeTypeId = consts.ATTRIBUTE_TYPE_IDS.FREQUENT_FLYERS;
+const preferredAirportAttributeTypeId = consts.ATTRIBUTE_TYPE_IDS.PREFERRED_AIRPORT;
 
 export default class TravelInformationController {
   /* @ngInject() */
-  constructor($rootScope, Validation, AttributeTypeService, TravelInformationService, $state) {
+  constructor($rootScope, Validation, AttributeTypeService, PreferredAirportService, TravelInformationService, $state) {
     this.$rootScope = $rootScope;
     this.validation = Validation;
     this.travelInformation = TravelInformationService;
     this.attributeTypeService = AttributeTypeService;
+    this.preferredAirportService = PreferredAirportService;
     this.state = $state;
 
     this.now = null;
@@ -17,6 +21,9 @@ export default class TravelInformationController {
     this.person = {};
     this.travelInfoForm = {};
     this.frequentFlyers = [];
+    this.commonAirportNames = null;
+    this.preferredAirportAttributeType = undefined;
+    this.preferredAirportAttribute = undefined;
     this.validPassport = null;
     this.preferredAirport = null;
     this.otherAirport = null;
@@ -36,11 +43,16 @@ export default class TravelInformationController {
 
     this.person = this.travelInformation.getPerson();
 
+    this.commonAirportNames = this.preferredAirportService.GetCommonAirportNames();
+    var userPreferredAirportName = this.preferredAirportService.GetPreferredAirportName(this.person);
+    this.preferredAirport = this.preferredAirportService.GetCommonAirportNameOrOther(userPreferredAirportName, this.commonAirportNames);
+    this.otherAirport = userPreferredAirportName;
+
     if (this.person.passportNumber) {
       this.validPassport = 'true';
     }
 
-    this.attributeTypeService.AttributeTypes().get({ id: attributeType }, (data) => {
+    this.attributeTypeService.AttributeTypes().get({ id: frequentFlyerAttributeTypeId }, (data) => {
       this.frequentFlyers = data.attributes.map((ff) => {
         const exists = this.frequentFlyerValue(ff.attributeId);
         if (exists) {
@@ -52,6 +64,17 @@ export default class TravelInformationController {
     }, (err) => {
       this.error = err;
     });
+
+    // Get preferred airport attribute
+    this.attributeTypeService.AttributeTypes().get({ id: preferredAirportAttributeTypeId }, (preferredAirportAttributeType) => {
+      this.preferredAirportAttributeType = preferredAirportAttributeType;
+      if (preferredAirportAttributeType.attributes.length > 0) {
+        this.preferredAirportAttribute =
+          preferredAirportAttributeType.attributes.find(a => a.attributeId === consts.ATTRIBUTE_IDS.PREFERRED_AIRPORT_NAME);
+      }
+      }, (err) => {
+        this.error = err;
+      });
   }
 
   passportInvalidContent() {
@@ -66,7 +89,7 @@ export default class TravelInformationController {
   }
 
   frequentFlyerValue(id) {
-    const frequentFlyerTypes = this.person.attributeTypes[attributeType];
+    const frequentFlyerTypes = this.person.attributeTypes[frequentFlyerAttributeTypeId];
     if (frequentFlyerTypes !== null && frequentFlyerTypes.attributes) {
       const currff = frequentFlyerTypes.attributes.find(ff => ff.attributeId === id);
       if (currff.selected) {
@@ -91,9 +114,18 @@ export default class TravelInformationController {
     if (this.travelInfoForm.$valid) {
       // set the selected attribute on frequent flyer..
       const flyers = this.buildFrequentFlyers();
-      this.person.attributeTypes[attributeType] = {
+      this.person.attributeTypes[frequentFlyerAttributeTypeId] = {
         attributes: flyers
       };
+
+      let preferredAirportName = this.preferredAirport === 'Other' ? this.otherAirport : this.preferredAirport;
+
+      this.preferredAirportAttribute =
+        this.preferredAirportService.SetAttributeNotesAndStartDate(this.preferredAirportAttribute, preferredAirportName);
+
+      this.person.attributeTypes[preferredAirportAttributeTypeId] = this.preferredAirportAttributeType;
+      delete this.person.singleAttributes[preferredAirportAttributeTypeId];
+
       // save the info
       this.travelInformation.profile.save(this.person, () => {
         // clear the current user from TravelInformationService
