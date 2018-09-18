a270326a0796884c27a6e999c4aeb3659db4e870
Chris Branch <chris.branch@ingagepartners.com>
2018-09-18 09:13:49 -0400

    Add try/catch and rollback

--- CI/SQL/20180917_160000_US14809-donor-statement-method.sql
+++ CI/SQL/20180917_160000_US14809-donor-statement-method.sql
@@ -43,79 +43,87 @@ BEGIN
         )
     ;
 
-    BEGIN TRAN
+    BEGIN TRY
+        BEGIN TRAN
 
-    DECLARE @Data_Changes TABLE (
-        Donor_ID INT NOT NULL,
-        New_Statement_Frequency_ID INT NOT NULL,
-        New_Statement_Method_ID INT NOT NULL,
-        Old_Statement_Frequency_ID INT NOT NULL,
-        Old_Statement_Method_ID INT NOT NULL,
+        DECLARE @Data_Changes TABLE (
+            Donor_ID INT NOT NULL,
+            New_Statement_Frequency_ID INT NOT NULL,
+            New_Statement_Method_ID INT NOT NULL,
+            Old_Statement_Frequency_ID INT NOT NULL,
+            Old_Statement_Method_ID INT NOT NULL,
 
-        PRIMARY KEY(Donor_ID)
-    );
+            PRIMARY KEY(Donor_ID)
+        );
 
-    -- Update Donor records to modify Statement Frequency and Statement Method
-    UPDATE Donors
-    SET
-        Statement_Frequency_ID = 1,
-        Statement_Method_ID = 2
-    OUTPUT
-        INSERTED.Donor_ID,
-        INSERTED.Statement_Frequency_ID,
-        INSERTED.Statement_Method_ID,
-        DELETED.Statement_Frequency_ID,
-        DELETED.Statement_Method_ID
-    INTO @Data_Changes
-    WHERE
-        Donor_ID IN (SELECT Donor_ID FROM @Donor_Changes)
-    ;
+        -- Update Donor records to modify Statement Frequency and Statement Method
+        UPDATE Donors
+        SET
+            Statement_Frequency_ID = 1,
+            Statement_Method_ID = 2
+        OUTPUT
+            INSERTED.Donor_ID,
+            INSERTED.Statement_Frequency_ID,
+            INSERTED.Statement_Method_ID,
+            DELETED.Statement_Frequency_ID,
+            DELETED.Statement_Method_ID
+        INTO @Data_Changes
+        WHERE
+            Donor_ID IN (SELECT Donor_ID FROM @Donor_Changes)
+        ;
 
-    DECLARE @Audit_Records crds_Audit_Item;
+        DECLARE @Audit_Records crds_Audit_Item;
 
-    -- Generate audit log entries for changes to Statement Method and/or Statement Frequency
-    INSERT INTO @Audit_Records
-        (Table_Name, Record_ID, Audit_Description, Field_Name, Field_Label, Previous_Value, New_Value, Previous_ID, New_ID)
-    SELECT
-        'Donors',
-        dc.Donor_ID,
-        'Updated',
-        'Statement_Frequency_ID',
-        'Stmt Frequency',
-        sf1.Statement_Frequency,
-        sf2.Statement_Frequency,
-        dc.Old_Statement_Frequency_ID,
-        dc.New_Statement_Frequency_ID
-    FROM
-        @Data_Changes dc
-        LEFT JOIN Statement_Frequencies sf1 on sf1.Statement_Frequency_ID = dc.Old_Statement_Frequency_ID
-        LEFT JOIN Statement_Frequencies sf2 on sf2.Statement_Frequency_ID = dc.New_Statement_Frequency_ID
-    WHERE
-        dc.Old_Statement_Frequency_ID <> dc.New_Statement_Frequency_ID
-    UNION
-    SELECT
-        'Donors',
-        dc.Donor_ID,
-        'Updated',
-        'Statement_Method_ID',
-        'Stmt Method',
-        sm1.Statement_Method,
-        sm2.Statement_Method,
-        dc.Old_Statement_Method_ID,
-        dc.New_Statement_Method_ID
-    FROM
-        @Data_Changes dc
-        LEFT JOIN Statement_Methods sm1 on sm1.Statement_Method_ID = dc.Old_Statement_Method_ID
-        LEFT JOIN Statement_Methods sm2 on sm2.Statement_Method_ID = dc.New_Statement_Method_ID
-    WHERE
-        dc.Old_Statement_Method_ID <> dc.New_Statement_Method_ID
-    ORDER BY
-        dc.Donor_ID
-    ;
+        -- Generate audit log entries for changes to Statement Method and/or Statement Frequency
+        INSERT INTO @Audit_Records
+            (Table_Name, Record_ID, Audit_Description, Field_Name, Field_Label, Previous_Value, New_Value, Previous_ID, New_ID)
+        SELECT
+            'Donors',
+            dc.Donor_ID,
+            'Updated',
+            'Statement_Frequency_ID',
+            'Stmt Frequency',
+            sf1.Statement_Frequency,
+            sf2.Statement_Frequency,
+            dc.Old_Statement_Frequency_ID,
+            dc.New_Statement_Frequency_ID
+        FROM
+            @Data_Changes dc
+            LEFT JOIN Statement_Frequencies sf1 on sf1.Statement_Frequency_ID = dc.Old_Statement_Frequency_ID
+            LEFT JOIN Statement_Frequencies sf2 on sf2.Statement_Frequency_ID = dc.New_Statement_Frequency_ID
+        WHERE
+            dc.Old_Statement_Frequency_ID <> dc.New_Statement_Frequency_ID
+        UNION
+        SELECT
+            'Donors',
+            dc.Donor_ID,
+            'Updated',
+            'Statement_Method_ID',
+            'Stmt Method',
+            sm1.Statement_Method,
+            sm2.Statement_Method,
+            dc.Old_Statement_Method_ID,
+            dc.New_Statement_Method_ID
+        FROM
+            @Data_Changes dc
+            LEFT JOIN Statement_Methods sm1 on sm1.Statement_Method_ID = dc.Old_Statement_Method_ID
+            LEFT JOIN Statement_Methods sm2 on sm2.Statement_Method_ID = dc.New_Statement_Method_ID
+        WHERE
+            dc.Old_Statement_Method_ID <> dc.New_Statement_Method_ID
+        ORDER BY
+            dc.Donor_ID
+        ;
 
-    DECLARE @date DATETIME = GETDATE();
-    EXEC crds_Add_Audit_Items @Audit_Records, @date, 'Svc Mngr', 0;
+        DECLARE @date DATETIME = GETDATE();
+        EXEC crds_Add_Audit_Items @Audit_Records, @date, 'Svc Mngr', 0;
 
-    COMMIT TRAN
+        COMMIT TRAN
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRAN
+
+        PRINT 'crds_service_update_donor_statement_parameters failed: ' + COALESCE(ERROR_MESSAGE(), '');
+    END CATCH
 END
 GO
