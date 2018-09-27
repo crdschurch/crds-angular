c16c4a31bcbd04ce1c7d966a4dece96ddab89ec8
vchtel <VictorChtelmakh@gmail.com>
2018-09-27 11:15:18 -0400

    Only events with waivers on the report please!

--- CI/SQL/20180927_103000_DE5957-Modify-Waiver-Report-Stored-Proc.sql
+++ CI/SQL/20180927_103000_DE5957-Modify-Waiver-Report-Stored-Proc.sql
@@ -20,16 +20,12 @@ ALTER PROCEDURE [dbo].[report_filter_camp_events_crossroads]
 AS
     
 	DECLARE @Domain_ID int = (SELECT Domain_ID FROM dp_Domains WHERE CAST(Domain_GUID as varchar(40)) = @DomainID)
-	DECLARE @EventType int = ISNULL((SELECT TOP 1 CS.Value 
-									 FROM dp_Configuration_Settings CS 
-									 WHERE Key_Name = 'CampEventTypeID' AND Application_Code = 'SSRS' AND CS.Domain_ID = @Domain_ID),8)
 
 	SELECT DISTINCT E.Event_ID, E.Event_Title
 	FROM Events AS E
 	LEFT JOIN cr_Event_Waivers AS W
 	ON E.Event_ID = W.Event_ID
-	WHERE (E.Event_Type_ID = @EventType OR W.Event_Waiver_ID IS NOT NULL)
-		  AND E.Domain_ID = @Domain_ID
+	WHERE W.Event_Waiver_ID IS NOT NULL AND E.Domain_ID = @Domain_ID
 	ORDER BY Event_Title;
 	
 GO
