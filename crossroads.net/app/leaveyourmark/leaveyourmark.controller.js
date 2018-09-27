2fb733413813ef0a4ee52be783cba33adc73dda1
tatelucas <tate.lucas@gmail.com>
2018-09-27 09:34:36 -0400

    two campaigns for leaveyourmark

--- crossroads.net/app/leaveyourmark/leaveyourmark.controller.js
+++ crossroads.net/app/leaveyourmark/leaveyourmark.controller.js
@@ -20,6 +20,22 @@
     ) {
         var vm = this;
 
+        vm.campaigns = Array;
+
+        for (let i=0; i<=1; i++) {
+          vm.campaigns[i] = new Array;
+          vm.campaigns[i]['currentDay'] = undefined;
+          vm.campaigns[i]['totalDays'] = undefined;
+          vm.campaigns[i]['given'] = undefined;
+          vm.campaigns[i]['committed'] = undefined;
+          vm.campaigns[i]['givenGercentage'] = undefined
+          vm.campaigns[i]['notStartedPercent'] = undefined
+          vm.campaigns[i]['behindPercent'] = undefined
+          vm.campaigns[i]['onPacePercent'] = undefined
+          vm.campaigns[i]['aheadPercent'] = undefined
+          vm.campaigns[i]['completedPercent'] = undefined
+        }
+
         vm.currentDay = undefined;
         vm.totalDays = undefined;
         vm.given = undefined;
@@ -36,20 +52,31 @@
 
         function activate() {
             LeaveYourMark.campaignSummary
-                         .get({pledgeCampaignId: 1103})
+                         .query({pledgeCampaignId: 1103})
                          .$promise
                          .then((data) => {
-                            vm.viewReady = true;
-                            vm.currentDay = data.currentDay;
-                            vm.totalDays = data.totalDays;
-                            vm.given = data.totalGiven;
-                            vm.committed = data.totalCommitted;
-                            vm.givenPercentage = $filter('number')(vm.given / vm.committed * 100, 0);
-                            vm.notStartedPercent = data.notStartedPercent;
-                            vm.behindPercent = data.behindPercent;
-                            vm.onPacePercent = data.onPacePercent;
-                            vm.aheadPercent = data.aheadPercent;
-                            vm.completedPercent = data.completedPercent;
+                             if(data && data.length && data.length>0) {
+                               data.forEach(function(element, i) {
+                                 // console.log(element);
+                                 // console.log(i);
+                                 vm.viewReady = true;
+                                 vm.campaigns[i]['currentDay'] = element.currentDay;
+                                 vm.campaigns[i]['totalDays'] = element.totalDays;
+                                 vm.campaigns[i]['given'] = element.totalGiven;
+                                 vm.campaigns[i]['committed'] = element.totalCommitted;
+                                 vm.campaigns[i]['givenPercentage'] = $filter('number')(vm.campaigns[i]['given']  / vm.campaigns[i]['committed'] * 100, 0);
+                                 vm.campaigns[i]['notStartedPercent'] = element.notStartedPercent;
+                                 vm.campaigns[i]['behindPercent'] = element.behindPercent;
+                                 vm.campaigns[i]['onPacePercent'] = element.onPacePercent;
+                                 vm.campaigns[i]['aheadPercent'] = element.aheadPercent;
+                                 vm.campaigns[i]['completedPercent'] = element.completedPercent;
+                                 console.log(vm.campaigns[i]);
+                                });
+
+                             }
+
+
+
                          })
                          .catch((err) => {
                             vm.viewReady = true;
