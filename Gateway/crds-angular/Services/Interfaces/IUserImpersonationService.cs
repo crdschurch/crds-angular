e4163bd72e51678f80f7d6535c02ca12c9a25f0d
dillon-courts <dillon.courts@gmail.com>
2019-02-05 16:29:08 -0500

    updaing parameter name

--- Gateway/crds-angular/Services/Interfaces/IUserImpersonationService.cs
+++ Gateway/crds-angular/Services/Interfaces/IUserImpersonationService.cs
@@ -13,19 +13,19 @@ namespace crds_angular.Services.Interfaces
         /// </summary>
         /// <typeparam name="TOutput">The output type of the 'action' function</typeparam>
         /// <param name="authDTO">The authDTO of the logged-in user, must have the "Can Impersonate" property set in order to impersonate.</param>
-        /// <param name="useridToImpersonate">The user id of the user to impersonate, typically the user's email address.</param>
+        /// <param name="usernameToImpersonate">The user id of the user to impersonate, typically the user's email address.</param>
         /// <param name="action">The action to run as the impersonated user</param>
         /// <returns>The output of the 'action' function</returns>
-        TOutput WithImpersonation<TOutput>(AuthDTO authDTO, string useridToImpersonate, Func<TOutput> action);
+        TOutput WithImpersonation<TOutput>(AuthDTO authDTO, string usernameToImpersonate, Func<TOutput> action);
 
         /// <summary>
         /// Execute the given function while impersonating another MinistryPlatform user.
         /// </summary>
         /// <typeparam name="TOutput">The output type of the 'action' function</typeparam>
         /// <param name="accessToken">The accessToken of the logged-in user, must have the "Can Impersonate" property set in order to impersonate.</param>
-        /// <param name="useridToImpersonate">The user id of the user to impersonate, typically the user's email address.</param>
+        /// <param name="usernameToImpersonate">The user id of the user to impersonate, typically the user's email address.</param>
         /// <param name="action">The action to run as the impersonated user</param>
         /// <returns>The output of the 'action' function</returns>
-        TOutput WithImpersonation<TOutput>(string accessToken, string useridToImpersonate, Func<TOutput> action);
+        TOutput WithImpersonation<TOutput>(string accessToken, string usernameToImpersonate, Func<TOutput> action);
     }
 }
