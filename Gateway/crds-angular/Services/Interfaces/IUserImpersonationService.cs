49d7641eeeb17700309de03f1d2c6b4c11f95090
dillon-courts <dillon.courts@gmail.com>
2019-02-05 16:26:45 -0500

    comment fix

--- Gateway/crds-angular/Services/Interfaces/IUserImpersonationService.cs
+++ Gateway/crds-angular/Services/Interfaces/IUserImpersonationService.cs
@@ -12,7 +12,7 @@ namespace crds_angular.Services.Interfaces
         /// Execute the given function while impersonating another MinistryPlatform user.
         /// </summary>
         /// <typeparam name="TOutput">The output type of the 'action' function</typeparam>
-        /// <param name="accessToken">The accessToken of the logged-in user, must have the "Can Impersonate" property set in order to impersonate.</param>
+        /// <param name="authDTO">The authDTO of the logged-in user, must have the "Can Impersonate" property set in order to impersonate.</param>
         /// <param name="useridToImpersonate">The user id of the user to impersonate, typically the user's email address.</param>
         /// <param name="action">The action to run as the impersonated user</param>
         /// <returns>The output of the 'action' function</returns>
