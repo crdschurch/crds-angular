5e0d3cf1ce259cecb13977b4598a2491bc7d5eaf
Jimmy Janssen <jjanssen@csmbp0010-jj.lan>
2019-02-13 16:48:08 -0500

    removing search route

--- crossroads.net/app/search/search.routes.js
+++ crossroads.net/app/search/search.routes.js
@@ -7,27 +7,6 @@
 
   function SearchRoutes($stateProvider, $urlMatcherFactory, $locationProvider) {
 
-    $stateProvider
-    .state('search', {
-      parent: 'noSideBar',
-      url: '/search?type&q',
-      controller: 'SearchController as search',
-      templateUrl: 'search/search-results.html',
-      data: {
-        type: '',
-        renderLegacyStyles: false
-      },
-      resolve:{
-        $stateParams: '$stateParams',
-        type: function($stateParams) {
-          return $stateParams.type;
-        },
-
-        searchString: function($stateParams) {
-          return $stateParams.q;
-        }
-      }
-    });
   }
 
 })();
