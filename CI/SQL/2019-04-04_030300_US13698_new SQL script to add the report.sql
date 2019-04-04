dacb8ebfb4f603ae0afe20672c59393fb7502317
shakila-rajaiah <shakila.rajaiah@crossroads.net>
2019-04-04 15:26:41 -0400

    US13698 FI Report Script

---
+++ CI/SQL/2019-04-04_030300_US13698_new SQL script to add the report.sql
@@ -0,0 +1,34 @@
+USE MinistryPlatform
+GO
+
+DECLARE @REPORT_ID int = 331
+DECLARE @DONORS_PAGE_ID int = 322;
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[dp_Reports] WHERE [Report_ID] = @REPORT_ID)
+BEGIN
+    SET IDENTITY_INSERT [dbo].[dp_Reports] ON
+    INSERT INTO [dbo].[dp_Reports] (
+   	  [Report_ID]
+   	 ,[Report_Name]
+   	 ,[Description]
+   	 ,[Report_Path]
+   	 ,[Pass_Selected_Records]
+   	 ,[Pass_LinkTo_Records]
+   	 ,[On_Reports_Tab]
+    ) VALUES (
+   	  @REPORT_ID
+   	 ,N'Weekend Service - FI Report'
+   	 ,N'Generate a list of volunteers to serve with the FI Teams.'
+   	 ,N'/Crossroads/Weekend Service Crossroads_FI_report-server'
+   	 ,0
+   	 ,0
+   	 ,1
+    )
+    SET IDENTITY_INSERT [dbo].[dp_Reports] OFF
+END
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[dp_Report_Pages] WHERE [Report_ID] = @REPORT_ID AND [Page_ID] = @DONORS_PAGE_ID)
+BEGIN
+    INSERT INTO [dbo].[dp_Report_Pages] VALUES (@REPORT_ID, @DONORS_PAGE_ID)
+END
+
