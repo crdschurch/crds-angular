697f06031c9b4660290c75bc0e2d4e31ccb3148c
Phil Lachmann <phil.lachmann@ingagepartners.com>
2019-05-17 09:08:05 -0400

    1 existence check'

--- CI/SQL/20190507_120000_US17119-huddle-sqlagent-job.sql
+++ CI/SQL/20190507_120000_US17119-huddle-sqlagent-job.sql
@@ -10,10 +10,7 @@ IF(NOT EXISTS(SELECT job_id, [name] FROM msdb.dbo.sysjobs where name=@JOBNAME))
 BEGIN
 	EXEC dbo.sp_add_job  
 		@job_name = @JOBNAME ;   
-END
 
-IF(NOT EXISTS(SELECT step_id,step_name,command FROM msdb.dbo.sysjobsteps WHERE step_name=@STEPNAME and command = @COMMAND))
-BEGIN
 	EXEC sp_add_jobstep  
 		@job_name = @JOBNAME,  
 		@step_name =@STEPNAME,  
@@ -21,16 +18,13 @@ BEGIN
 		@command = @COMMAND,   
 		@retry_attempts = 2,  
 		@retry_interval = 5 ;  
-END
 
-IF(NOT EXISTS(SELECT * FROM msdb.dbo.sysschedules WHERE name=@SCHEDULENAME))
-BEGIN
-EXEC msdb.dbo.sp_add_jobschedule 
-	@job_name = @JOBNAME, -- Job name
-	@name = @SCHEDULENAME,  -- Schedule name
-	@freq_type = 4, -- Daily
-	@freq_interval = 1, 
-	@active_start_time = 020000 -- 2:00 AM
+	EXEC msdb.dbo.sp_add_jobschedule 
+		@job_name = @JOBNAME, -- Job name
+		@name = @SCHEDULENAME,  -- Schedule name
+		@freq_type = 4, -- Daily
+		@freq_interval = 1, 
+		@active_start_time = 020000 -- 2:00 AM
 END
 
 GO
