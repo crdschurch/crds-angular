d5e5860c55ae86239a29620e5f1176ae308fd05b
Brian Cavanaugh <bcavanaugh@callibrity.com>
2019-05-17 08:39:17 -0400

    Update script

--- CI/SQL/20190516_140000_US17191-update-and-add-page-views-journey-chaser-groups.sql
+++ CI/SQL/20190516_140000_US17191-update-and-add-page-views-journey-chaser-groups.sql
@@ -10,9 +10,13 @@ UPDATE dp_Page_Views
 SET View_Title = 'Current Chaser Participants'
 WHERE Page_View_ID = 1130
 
+GO
+
 UPDATE dp_Page_Views
 SET View_Title = 'Current Chaser Leaders'
 WHERE Page_View_ID = 93316
+
+GO
 
 INSERT INTO [dbo].[dp_Page_Views]
 	([View_Title], [Page_ID]
@@ -25,6 +29,8 @@ INSERT INTO [dbo].[dp_Page_Views]
 	, 'Group_ID_Table.[End_Date] IS NULL AND Group_Participants.[End_Date] IS NULL AND Group_Role_ID_Table.[Group_Role_ID] != 22 AND EXISTS (Select group_ID from GROUP_ATTRIBUTES where group_id = group_participants.group_id and attribute_id = (select top 1 attribute_id from attributes where attribute_category_id = 51 order by start_date desc))'
 	)
 
+GO
+
 INSERT INTO [dbo].[dp_Page_Views]
 	([View_Title], [Page_ID]
 	,[Field_List]
@@ -35,3 +41,5 @@ INSERT INTO [dbo].[dp_Page_Views]
 	, 'Participant_ID_Table_Contact_ID_Table.[Display_Name] AS [Display Name] , Group_ID_Table.[Group_Name] AS [Group Name] , Group_ID_Table.[Available_Online] AS [Public Flag] , Participant_ID_Table_Group_Leader_Status_ID_Table.[Group_Leader_Status] AS [Group Leader Status] , Participant_ID_Table_Contact_ID_Table_Household_ID_Table_Congregation_ID_Table.[Congregation_Name] AS [Congregation Name] , Group_ID_Table_Group_Type_ID_Table.[Group_Type] AS [Group Type] , Group_ID_Table.[Start_Date] AS [Start Date] , Group_Role_ID_Table.[Role_Title] AS [Role Title] , Group_Participants.[Group_Participant_ID] AS [Group Participant ID]'
 	, 'Group_ID_Table.[End_Date] IS NULL AND Group_Participants.[End_Date] IS NULL AND Group_Role_ID_Table.[Group_Role_ID] != 22 AND EXISTS (Select group_ID from GROUP_ATTRIBUTES where group_id = group_participants.group_id and attribute_id = (select top 1 attribute_id from attributes where attribute_category_id = 51 order by start_date desc))'
 	)
+
+GO
