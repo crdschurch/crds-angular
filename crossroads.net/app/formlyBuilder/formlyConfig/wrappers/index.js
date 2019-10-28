d0a299bce89cef252e1359422c278ea3de50cf85
Ryan Donnelly <>
2019-10-28 14:44:49 -0400

    Removing crossroads.net folder and netlify documents

--- crossroads.net/app/formlyBuilder/formlyConfig/wrappers/index.js
+++
@@ -1,6 +0,0 @@
-export default ngModule => {
-  require('./formlyBuilder-show-error')(ngModule);
-  require('./formlyBuilder-label')(ngModule);
-  require('./formlyBuilder-has-error')(ngModule);
-  require('./formlyBuilder-show-alert')(ngModule);
-}
--- crossroads.net/app/go_volunteer/projectCard/goVolunteerProjectCard.controller.js
+++
@@ -1,7 +0,0 @@
-export default class GoVolunteerAnywhereProfileController {
-  /* ngInject */
-  constructor(){}
-  $onInit() {
-
-  }
-}
