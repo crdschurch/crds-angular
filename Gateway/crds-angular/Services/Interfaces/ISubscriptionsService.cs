c21311f846e4eedbf5360ce6f03378070e5ca12e
Ryan Donnelly <rdonnelly02@gmail.com>
2020-09-24 15:23:46 -0400

    Remove Subscription controller and related files

--- Gateway/crds-angular/Services/Interfaces/ISubscriptionsService.cs
+++ Gateway/crds-angular/Services/Interfaces/ISubscriptionsService.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using crds_angular.Models.Crossroads;
-using crds_angular.Models.Crossroads.Subscription;
+﻿using System.Collections.Generic;
 
 namespace crds_angular.Services.Interfaces
 {
     public interface ISubscriptionsService
     {
-        List<Dictionary<string, object>> GetSubscriptions(int contactId);
-
         int SetSubscriptions(Dictionary<string, object> subscription, int contactId);
-
-        OptInResponse AddListSubscriber(string emailAddress, string listName);
     }
 }
--- Gateway/crds-angular/Services/SubscriptionsService.cs
+++ Gateway/crds-angular/Services/SubscriptionsService.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using crds_angular.Services.Interfaces;
 using MPInterfaces = MinistryPlatform.Translation.Repositories.Interfaces;
-using crds_angular.Models.Crossroads.Subscription;
-using Crossroads.Web.Common;
 using Crossroads.Web.Common.MinistryPlatform;
 
 namespace crds_angular.Services
@@ -19,31 +17,6 @@ namespace crds_angular.Services
             _emailListHandler = emailListHandler;
             _apiUserService = apiUserService;
         }
-		public List<Dictionary<string, object>> GetSubscriptions(int contactId)
-        {
-			var token = _apiUserService.GetDefaultApiClientToken();
-            var publications = _ministryPlatformService.GetRecordsDict("Publications", token, ",,,,True", "8 asc");
-            var subscriptions = _ministryPlatformService.GetSubPageRecords("SubscriptionsSubPage", contactId, token);
-            foreach (var publication in publications)
-            {
-                object unSub = true;
-                foreach (var subscription in subscriptions)
-                {
-                    object spID;
-                    object pID;
-                    subscription.TryGetValue("Publication_ID", out spID);
-                    publication.TryGetValue("ID", out pID);
-                    if ((int)pID == (int)spID)
-                    {
-                        subscription.TryGetValue("Unsubscribed", out unSub);
-                        publication.Add("Subscription", subscription);
-                        break;
-                    }
-                }
-                publication.Add("Subscribed", !(bool)unSub);
-            }
-            return publications;
-        }
 
         public int SetSubscriptions(Dictionary<string, object> subscription, int contactId)
         {
@@ -56,12 +29,6 @@ namespace crds_angular.Services
                 return Convert.ToInt32(spID);
             }
 			return _ministryPlatformService.CreateSubRecord("SubscriptionsSubPage", contactId, subscription, token);
-        }
-
-        public OptInResponse AddListSubscriber(string emailAddress, string listName)
-        {
-            var token = _apiUserService.GetDefaultApiClientToken();
-            return _emailListHandler.AddListSubscriber(emailAddress, listName, token);
         }
     }
 }
