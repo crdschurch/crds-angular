64481953d9dbc0677c20eecfa0fc95a5d57be4f0
Sarah Henney <shenney@callibrity.com>
2020-10-16 13:48:13 -0400

    Added more password request reset tests, finalized Vault location, centralized users

--- Gateway/crds-angular.api_test/cypress/integration/LoginController/POST_login_prodspec.ts
+++ Gateway/crds-angular.api_test/cypress/integration/LoginController/POST_login_prodspec.ts
@@ -1,28 +1,26 @@
-import { TestConfig, TestCase, unzipTests } from "../../shared/test_scenario_factory";
+import { TestConfig, TestCase, unzipTests } from "cypress/shared/test_scenario_factory";
+import { Ben } from "cypress/shared/users";
 import { badRequestContract, badRequestProperties } from "./schemas/badRequest";
 import { mpLoginBasicAuthContract, mpLoginSchemaProperties } from "./schemas/loginResponse";
 
 // Data Setup
-const username = "mpcrds+auto+2@gmail.com";
-const password = Cypress.env("BEN_KENOBI_PW");
-
 const testConfig: TestConfig[] = [
   {
-    setup: { description: "Valid User", body: { username, password } },
+    setup: { description: "Valid User", body: { username: Ben.email, password: Ben.password } },
     result: {
       status: 200,
       body: {
         schemas: [mpLoginSchemaProperties, mpLoginBasicAuthContract],
-        properties: [{ name: "userEmail", value: username }]
+        properties: [{ name: "userEmail", value: Ben.email }]
       }
     }
   },
   {
     setup: [
-      { description: "Incorrect Password", body: { username, password: "bad" } },
-      { description: "Missing Password", body: { username } },
-      { description: "Missing Username", body: { password } },
-      { description: "User Doesn't Exist", body: { username: "fakeUser@fakemail.wxyz", password } },
+      { description: "Incorrect Password", body: { username: Ben.email, password: "bad" } },
+      { description: "Missing Password", body: { username: Ben.email } },
+      { description: "Missing Username", body: { password: Ben.password } },
+      { description: "User Doesn't Exist", body: { username: "fakeUser@fakemail.wxyz", password: Ben.password } },
       { description: "Body Undefined", body: undefined },
       { description: "Body Null", body: null },
       { description: "Body Empty String", body: "" }
--- Gateway/crds-angular.api_test/cypress/integration/LoginController/POST_requestpasswordreset_mobile_spec.ts
+++ Gateway/crds-angular.api_test/cypress/integration/LoginController/POST_requestpasswordreset_mobile_spec.ts
@@ -1,5 +1,5 @@
 import { TestCase, TestConfig, unzipTests } from "cypress/shared/test_scenario_factory";
-import { Ben, KeeperJr, Load } from "cypress/shared/users";
+import { Ben } from "cypress/shared/users";
 import { badRequestContract, badRequestProperties } from "./schemas/badRequest";
 
 // Data Setup
@@ -11,7 +11,7 @@ const testConfig: TestConfig[] = [
   {
     setup: [
       { description: "Person Doesn't Exist", body: { email: "this_email_should_not_exist@fake_emails.io" } },
-      { description: "Person has Contact Record but no User Record", body: { email: Load.email } },
+      { description: "Person has Contact Record but no User Record", body: { email: "mpcrds+LoadTest_98@gmail.com" } },
       { description: "Invalid Email", body: { email: "{" } },
     ],
     result: { status: 200 }, //Not technically a bug but misleading to user
@@ -25,7 +25,7 @@ const testConfig: TestConfig[] = [
   },
   {
     setup: [
-      { description: "Email is Subset of Another Email (bug)", body: { email: KeeperJr.email } }
+      { description: "Email is Subset of Another Email (bug)", body: { email: "lia@differential.com" } }
     ],
     result: { status: 200 },
     preferredResult: { status: 200 } //Should still have valid output, but won't error on server side
@@ -60,12 +60,12 @@ const testConfig: TestConfig[] = [
 ];
 
 // Run Tests
-describe('POST /api/requestpasswordreset', () => {
+describe('POST /api/requestpasswordreset/mobile', () => {
   unzipTests(testConfig)
     .forEach((t: TestCase) => {
       it(t.title, () => {
         const mpRequestPasswordReset: Partial<Cypress.RequestOptions> = {
-          url: "/api/requestpasswordreset",
+          url: "/api/requestpasswordreset/mobile",
           method: "POST",
           body: t.setup.body,
           headers: t.setup.header,
