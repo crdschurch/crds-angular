1e2114ef2c2401e8e71cf6a0ab78589011e28feb
Sarah Henney <shenney@callibrity.com>
2021-01-11 18:21:22 -0500

    Created new test builder framework and tooling, converted all tests

--- Gateway/crds-angular.api_test/cypress/api_tests/LoginController/GET_verifyresettoken_spec.ts
+++ Gateway/crds-angular.api_test/cypress/api_tests/LoginController/GET_verifyresettoken_spec.ts
@@ -1,110 +1,79 @@
 import { setPasswordResetToken } from "shared/mp_api";
-import { unzipTests } from "shared/test_scenario_factory";
 import { Gatekeeper, } from 'shared/users';
 import { getUUID } from "shared/data_generator";
 import { mpVerifyResetTokenContract, mpVerifyResetTokenSchemaProperties } from "./schemas/verifyresettokenResponse";
-import { ResponseTypes } from "shared/enums";
+import { runTest, unzipScenarios } from "shared/CAT/cypress_api_tests";
 
 // Data Setup
-const testConfig:TestFactory.TestConfig[] = [
-  {
-    setup: [
-      {
-        description: "Valid Request",
-        data: {
-          token: getUUID()
-        },
-        setup: function () {
-          return setPasswordResetToken(Gatekeeper.email, this.data.token)
-        }
-      }
-    ],
-    result: { 
-      status: 200,
-      body: {
-        schemas: [mpVerifyResetTokenSchemaProperties, mpVerifyResetTokenContract],
+function setUrl(token: string, request: Partial<Cypress.RequestOptions>){
+  request.url = `${request.url}/${token}`;
+}
+
+const sharedRequest = {
+  urls: ["/api/verifyresettoken", "/api/v1.0.0/verify-reset-token"],
+  options: {
+    method: "GET",
+    failOnStatusCode: false
+  }
+}
+const successScenarios: CAT.CompactTestScenario = {
+  sharedRequest,
+  sharedResponse:{
+    schemas: [mpVerifyResetTokenSchemaProperties, mpVerifyResetTokenContract]
+  },
+  scenarios: [
+    {
+      description: "Valid Request",
+      request: {},
+      setup() {
+        const token = getUUID();
+        setUrl(token, this.request);
+        return setPasswordResetToken(Gatekeeper.email, token).then(() => this);
+      },
+      response: {
+        status: 200,
         properties: [{
           name: "TokenValid",
           value: true
-        }] 
+        }]
       }
-     }
-  },
-  {
-    setup: [
-      {
-        description: "Reset Token that Does Not Match any Reset Request",
-        data: {
-          token: '123FakeToken99876'
-        }
+    },
+    {
+      description: "Reset Token that Does Not Match any Reset Request",
+      request: {},
+      setup() {
+        const token = '123FakeToken99876';
+        setUrl(token, this.request);
+        return cy.wrap(this);
       },
-    ],
-    result: { 
-      status: 200,
-      body:{
-        schemas: [mpVerifyResetTokenSchemaProperties, mpVerifyResetTokenContract],
+      response: {
+        status: 200,
         properties: [{
           name: "TokenValid",
           value: false
-        }] 
+        }]
       }
-     }
-  },
-  {
-    setup: [      
-      {
-        description: "Reset Token is empty string",
-        data: {
-          token: ''
-        }
+    },
+  ]
+}
+const notFoundScenario: CAT.CompactTestScenario = {
+  sharedRequest,
+  scenarios: [
+    {
+      description: "Reset Token is empty string",
+      request: {},
+      setup(){
+        setUrl("", this.request);
+        return cy.wrap(this);
       },
-    ],
-    result:{
-      status: 404,
-      body: { 
-        contentType: ResponseTypes.text
+      response:{
+        status: 404
       }
-    }
-  }
-];
+    },
+  ]
+}
 
-// Run Tests
-describe('GET /api/verifyresettoken/{token}', () => {
-  unzipTests(testConfig)
-    .forEach((t: TestFactory.Test) => {
-      it(t.title, () => {
-        const mpVerifyResetToken: Partial<Cypress.RequestOptions> = {
-          url: `/api/verifyresettoken/${t.data.token}`,
-          method: "GET",
-          failOnStatusCode: false
-        };
-
-        t.setup() //Arrange
-          .then(() => cy.request(t.buildRequest(mpVerifyResetToken))) //Act
-          .verifyStatus(t.result.status) //Assert
-          .itsBody(t.result.body)
-          .verifySchema(t.result.body)
-          .verifyProperties(t.result.body);
-      });
-    });
-});
-
-describe('GET /api/v1.0.0/verify-reset-token/{token}', () => {
-  unzipTests(testConfig)
-    .forEach((t: TestFactory.Test) => {
-      it(t.title, () => {
-        const mpVerifyResetToken: Partial<Cypress.RequestOptions> = {
-          url: `/api/v1.0.0/verify-reset-token/${t.data.token}`,
-          method: "GET",
-          failOnStatusCode: false
-        };
-
-        t.setup() //Arrange
-          .then(() => cy.request(t.buildRequest(mpVerifyResetToken))) //Act
-          .verifyStatus(t.result.status) //Assert
-          .itsBody(t.result.body)
-          .verifySchema(t.result.body)
-          .verifyProperties(t.result.body);
-      });
-    });
-});
+describe("/Login/VerifyResetTokenRequest()", () => {
+  unzipScenarios(successScenarios).forEach(runTest);
+  unzipScenarios(notFoundScenario).forEach(runTest);
+})
