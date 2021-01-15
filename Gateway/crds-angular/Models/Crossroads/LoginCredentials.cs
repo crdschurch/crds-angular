f9cd1ae603b725db12d84ba4a6bf8d4832928dc5
Sarah Henney <shenney@callibrity.com>
2021-01-15 17:13:13 -0500

    Extracted models from LoginController into /Models/Crossroads Linting cleanup of LoginController and UserController

---
+++ Gateway/crds-angular/Models/Crossroads/LoginCredentials.cs
@@ -0,0 +1,8 @@
+﻿namespace crds_angular.Models.Crossroads
+{
+    public class LoginCredentials
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
